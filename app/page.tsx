96fe4fbed2718056b03887da8f67d6a41786f3e4
ariel7619 <olberg.ariel@gmail.com>
2024-10-16 10:59:07 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -193,6 +193,21 @@ export default function Home() {
             >
               Showcase
             </Link>
+            <Link
+              href={"/webdesign"}
+              className="
+              bg-white
+              py-3 
+              px-10
+              md:px-16
+              md:text-xl
+              border-4
+            border-black
+              rounded-[6px]
+              hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
+            >
+              Webdesign
+            </Link>
           </div>
 
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 items-center text-left md:justify-items-center md:mx-auto mt-10 md:mt-16">
