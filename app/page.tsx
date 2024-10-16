f2f3757bb0b719ac79c1e4332be13b5e6e529a6e
ariel7619 <olberg.ariel@gmail.com>
2024-10-16 11:35:33 +0200

    a

--- app/page.tsx
+++ app/page.tsx
@@ -22,38 +22,33 @@ const services = [
   {
     icon: "/images/s_6.png",
     title: "Web Design + Development",
-    description:
-      "Take your business to the next level with our web design and development services",
+    description: "Take your business to the next level with our web design and development services",
+    link: "/webdesign",
   } ,
   {
     icon: "/images/s_1.png",
     title: "Search Engine Optimization",
-    description:
-      "Get your website to the top of search engine results with our SEO services",
+    description: "Get your website to the top of search engine results with our SEO services",
   },
   {
     icon: "/images/s_5.png",
     title: "Content Creation",
-    description:
-      "Boost your brand's online presence with our social media marketing services",
+    description: "Boost your brand's online presence with our social media marketing services",
   },
   {
     icon: "/images/s_3.png",
     title: "Social Media Marketing",
-    description:
-      "Interact with your customers and increase sales with our email marketing services",
+    description: "Interact with your customers and increase sales with our email marketing services",
   },
   {
     icon: "/images/s_4.png",
     title: "Email Marketing",
-    description:
-      "With our content creation services, we help businesses drive results",
+    description: "With our content creation services, we help businesses drive results",
   },
   {
     icon: "/images/s_2.png",
     title: "Pay-Per-Click Advertising",
-    description:
-      "Don't waste money on ineffective advertising. Our PPC services help you reach your target audience",
+    description: "Don't waste money on ineffective advertising. Our PPC services help you reach your target audience",
   },
 ];
 
@@ -311,7 +306,6 @@ export default function Home() {
                 />
                 <h1 className="text-xl font-medium">{service.title}</h1>
                 <p className="text-gray-500">{service.description}</p>
-                <Link href="/">Home</Link>
               </div>
             ))}
           </div>
